b184fcd48aad890c6cc656ae5a609a27db194fdd
adnan106 <mdrakibulhasanadnan@gmail.com>
2023-06-09 18:49:26 +0600

    Homepage created by adnan

--- app/src/main/java/com/example/cardiacrecorder/LoginPage.java
+++ app/src/main/java/com/example/cardiacrecorder/LoginPage.java
@@ -2,13 +2,30 @@ package com.example.cardiacrecorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
+
+    Button loginbutton;
+    Button signupbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginbutton  = findViewById(R.id.btn_login);
+        signupbutton = findViewById(R.id.btn_signup);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginPage.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
